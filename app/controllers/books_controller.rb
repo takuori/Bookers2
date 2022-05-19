ce22a9f2d16e9bd754d5560908f62c935a62731e
EC2 Default User <ec2-user@ip-172-31-21-120.ec2.internal>
2022-05-19 07:56:24 +0000

    課題3bookのページはあと少し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,28 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   @book.save
+   redirect_to book_path(@book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
-  post "books" => "books#create"
 
 
   resources :books
