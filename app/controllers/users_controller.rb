65f71619227b1f861eb14bfd63ce120d80e3a8ca
EC2 Default User <ec2-user@ip-172-31-21-120.ec2.internal>
2022-05-25 00:41:29 +0000

    課題3エラーあと17個

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @users = User.all
     @user = current_user
@@ -6,11 +8,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if @user == current_user
-      render :edit
-    else
-      redirect_to edit_user_url
-    end
+
   end
 
   def show
@@ -32,5 +30,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
